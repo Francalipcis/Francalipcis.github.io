5812ed49d530452e1c733f3000251f17946391be
Your Name <you@example.com>
2022-01-12 00:04:31 -0300

    actualisacion de pagina2.0

--- weather-app/app/pronostico.js
+++ weather-app/app/pronostico.js
@@ -45,25 +45,6 @@ window.addEventListener('load', () => {
                     `;
                     pronosticList.appendChild(element);
                 }
-
-
-
-                for(var i = 5; i <= 10; i++){
-                    //crear pronostico
-                    let day = i
-                    let pronosticList = document.getElementById('pronostico_container');
-                    let element = document.createElement('div');
-                    element.innerHTML = `
-                        <h4 id="fecha_card${i}" class="fecha_c -hiden">${data.data[i].datetime}</h4>
-                            <div class="card${i} -hiden" id="card${i}">
-                                <img src="./img/icons/${data.data[i].weather.icon}.png" alt="" class="img_card" id="img_c_${i}">
-                                <h5 id="temp_c_${i}" class="temp_card">${Math.round(data.data[i].max_temp)}°C</h5>
-                                <h5 id="humidity_c_${i}" class=" humidity_card">${data.data[i].precip}%</h5>
-                                <h5 id="chance_c_${i}" class="change_card">${data.data[i].weather.description}</h5>
-                            </div>
-                    `;
-                    pronosticList.appendChild(element);
-                }
         
             })
         
@@ -71,51 +52,6 @@ window.addEventListener('load', () => {
     }
 });
 
-
-//cambiar de pagina
-document.getElementById('verPronostics1').onclick = () => {
-    
-    //agregando
-    for(let a = 1; a <= 5; a++){
-        document.getElementById(`card${a}`).classList.add('-hiden');
-        document.getElementById(`fecha_card${a}`).classList.add('-hiden');
-
-        document.getElementById(`card5`).style.visibility = "hidden";
-    }
-
-
-    //quitando
-    for(let b = 5; b <= 10; b++){
-        document.getElementById(`card${b}`).classList.remove('-hiden');
-        document.getElementById(`fecha_card${b}`).classList.remove('-hiden');
-
-        document.getElementById(`card5`).classList.remove('-hiden');
-        document.getElementById(`fecha_card5`).style.visibility = "hidden";
-    }
-
-};
-
-
-//volver pagina
-document.getElementById('verPronostics2').onclick = () => {
-    
-    //agregando
-    for(let b = 5; b <= 10; b++){
-        document.getElementById(`card${b}`).classList.add('-hiden');
-        document.getElementById(`fecha_card${b}`).classList.add('-hiden');
-    }
-
-
-    //quitando
-    for(let a = 1; a <= 5; a++){
-        document.getElementById(`card${a}`).classList.remove('-hiden');
-        document.getElementById(`fecha_card${a}`).classList.remove('-hiden');
-
-        document.getElementById(`card5`).style.visibility = "visible";
-        document.getElementById(`fecha_card5`).style.visibility = "visible";
-    }
-
-};
 
 
 
