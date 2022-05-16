3173eac71f4e17b1b315752a7fc2583efaacfe3e
Your Name <you@example.com>
2022-05-16 01:23:07 -0300

    actualizacion de pagina

---
+++ francalipciswebs/src/js/main.js
@@ -0,0 +1,84 @@
+
+//definiendo la timeline
+let tl = gsap.timeline();
+
+
+
+
+//animando la barra superior y los lincks
+tl.from('.menu-navegacion', {
+    duration: 2,
+
+    x: -2000,
+    ease:'power4'
+})
+
+
+
+tl.from('.linck', {
+    duration: .5,
+    
+    y: -200,
+    stagger:0.3
+})
+
+
+
+//animando el logo junto con el quienes somos
+gsap.registerPlugin(ScrollTrigger);
+
+let infoScrollTimeline = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.quienesSomos',
+        start: 'top +=440',
+        
+        ease:'power1'
+    },
+})
+infoScrollTimeline.from('#logo', {
+    duration: 2,
+    
+    opacity:0,
+    x:-250,
+    
+})
+
+
+
+//animando la seccion de contactos
+let contactScrollTimeline = gsap.timeline({
+    scrollTrigger: {
+        trigger: '#contacto',
+        start: 'top +=250',
+
+    }
+})
+
+contactScrollTimeline.from('.card', {
+    duration: .5,
+
+    opacity:0,
+    y:-100,
+    ease:'power4',
+
+    stagger:0.3
+})
+
+
+let pricesScrollTimeline = gsap.timeline({
+    scrollTrigger: {
+        trigger: '#servicesContainer',
+        start: 'top 500',
+    }
+})
+
+
+pricesScrollTimeline.to('#servicesContainer', {
+    duration:.3,
+
+    color: 'white',
+    letterSpacing: 3,
+    
+    backgroundColor: 'black',
+    border:'#009dff solid 4px',
+})
